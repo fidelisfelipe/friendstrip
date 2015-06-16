a38b9309117fc0f2aece7675be3304324bfd26f3
Fidelis Felipe <atosfiel@gmail.com>
2015-06-16 02:20:11 -0300

    test grunt

--- post_install.sh
+++ post_install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash ./node_modules/grunt-cli/bin/grunt build:production
 ./node_modules/bower/bin/bower install
+./node_modules/bower/bin/bower install grunt
+./node_modules/bower/bin/bower install grunt-cli
+./node_modules/grunt-cli/bin/grunt build:production
