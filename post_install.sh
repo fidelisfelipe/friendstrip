4374ed26bfe5b039e7af52d5b2c3bf66f3782bdc
Fidelis Felipe <atosfiel@gmail.com>
2015-06-16 22:24:49 -0300

    change sh for sequency install grunt and grunt-cli

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash ./node_modules/grunt-cli/bin/grunt build:production
 ./node_modules/bower/bin/bower install
-./node_modules/bower/bin/bower install grunt
-./node_modules/bower/bin/bower install grunt-cli
+./node_modules/bower/bin/bower install -g grunt-cli grunt
 ./bower_components/grunt-cli/bin/grunt
