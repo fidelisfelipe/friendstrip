960a5fe533f418d2103edd705b7a9b6e0d3f6773
Fidelis Felipe <atosfiel@gmail.com>
2015-06-16 22:46:49 -0300

    modify path grunt in package.json

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash ./node_modules/grunt-cli/bin/grunt build:production
 ./node_modules/bower/bin/bower install
-./node_modules/bower/bin/bower install -g grunt-cli grunt
+./node_modules/bower/bin/bower install -g grunt-cli
+./node_modules/bower/bin/bower install -g grunt
 ./bower_components/grunt-cli/bin/grunt
